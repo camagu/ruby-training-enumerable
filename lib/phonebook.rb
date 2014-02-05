26668f55b109540c5246e2ed009dc14feafe5a02
Felipe Serna <ofelipess@gmail.com>
2014-02-05 15:42:57 -0500

    Excercise 2 and refactor rspec for excercise 1

--- lib/phonebook.rb
+++ lib/phonebook.rb
@@ -14,4 +14,13 @@ class Phonebook
   	list_names
   end
 
+  def list_phone_numbers
+  	# As a client I want a list of the phone book contacts' phone numbers so I can display them to the user
+  	list_phone_numbers = []
+  	@contacts.each do |c|
+  		list_phone_numbers.push(c.phone_numbers)
+  	end
+  	list_phone_numbers
+  end
+
 end
--- spec/lib/phonebook_spec.rb
+++ spec/lib/phonebook_spec.rb
@@ -3,7 +3,7 @@ require './lib/phonebook'
 require './lib/contact'
 
 describe Phonebook do
-  let(:contact_1) { double('contact', name: 'first contact', phone_numbers: '5555555') }
+  let(:contact_1) { double('contact', name: 'first contact', phone_numbers: ['4444444','5555555']) }
   let(:contact_2) { double('contact', name: 'second contact', phone_numbers: '6666666') }
   let(:contacts) { [ contact_1, contact_2 ] }
   let(:phonebook) { Phonebook.new(contacts) }
@@ -17,7 +17,14 @@ describe Phonebook do
   describe "list_names" do
 	it { expect(phonebook).to respond_to(:list_names) }
   	it "should return a list with contact names" do
-		expect(phonebook.list_names).to eq(['first contact', 'second contact'])
+		expect(phonebook.list_names).to eq([contact_1.name, contact_2.name])
+  	end
+  end
+
+  describe "list_phone_numbers" do
+	it { expect(phonebook).to respond_to(:list_phone_numbers) }
+  	it "should return a list with contact phone numbers" do
+		expect(phonebook.list_phone_numbers).to eq([contact_1.phone_numbers, contact_2.phone_numbers])
   	end
   end
 end
