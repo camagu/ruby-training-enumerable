a6e2ea6ffc733ae3c658134eb8fd705fafadc6a6
Felipe Serna <ofelipess@gmail.com>
2014-02-06 14:28:55 -0500

    Fix Exercises, still an error on exercise 4.

--- lib/phonebook.rb
+++ lib/phonebook.rb
@@ -5,50 +5,28 @@ class Phonebook
     @contacts = contacts
   end
 
-  def list_names
-  	# As a client I want a list of the phone book contacts' names so I can display them to the user
-  	list_names = []
-  	@contacts.each do |c|
-  		list_names.push(c.name)
-  	end
-  	list_names
+  def contact_names
+  	contacts.map { |contact| contact.name }
   end
 
-  def list_phone_numbers
-  	# As a client I want a list of the phone book contacts' phone numbers so I can display them to the user
-  	list_phone_numbers = []
-  	@contacts.each do |c|
-  		list_phone_numbers.push(c.phone_numbers)
-  	end
-  	list_phone_numbers
+  def contact_phone_numbers
+  	contacts.flat_map { |contact| contact.phone_numbers }
   end
 
-  def list_complete_contacts
-  	# As a client I want a mapping of the phone book contacts' names to their phone numbers so I can display the contact's phone numbers under it's name
-  	list_complete_contacts = Hash.new
-  	@contacts.each do |c|
-	  	list_complete_contacts[c.name] = c.phone_numbers
+  def contact_name_and_phone_numbers
+  	contact_name_and_phone_numbers = {}
+  	contacts.map do |contact|
+	  	contact_name_and_phone_numbers[contact.name] = contact.phone_numbers
   	end
-  	list_complete_contacts
+  	contact_name_and_phone_numbers
   end
 
   def complete_contacts?
-  	incomplete = 0
-  	@contacts.each do |c|
-	  	if c.phone_numbers.nil? || c.phone_numbers.empty?
-	  		incomplete+=1
-	  	end
-  	end
-  	true unless incomplete > 0
+    contacts.any? { |contact| !contact.phone_numbers.nil? && !contact.phone_numbers.empty? }
   end
 
   def incomplete_contacts
-  	incomplete_contacts = []
-  	@contacts.each do |c|
-	  	if c.phone_numbers.nil? || c.phone_numbers.empty?
-	  		incomplete_contacts.push(c.name)
-	  	end
-  	end
-  	incomplete_contacts
+  	contacts.find_all { |contact| contact.phone_numbers.nil? || contact.phone_numbers.empty?  }
   end
+
 end
--- spec/lib/phonebook_spec.rb
+++ spec/lib/phonebook_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 require './lib/phonebook'
-require './lib/contact'
 
 describe Phonebook do
-  let(:c1) { double('contact', name: 'first contact', phone_numbers: ['4444444','5555555']) }
-  let(:c2) { double('contact', name: 'second contact', phone_numbers: '6666666') }
-  let(:contacts) { [ c1, c2 ] }
-  let(:phonebook) { Phonebook.new(contacts) }
+  let(:contact1)    { double('contact 1',   name: 'first contact',        phone_numbers: ['0000000', '1111111'] ) }
+  let(:contact2)    { double('contact 2',   name: 'second contact',       phone_numbers: ['2222222'] ) }
+  let(:incomplete)  { double('incomplete',  name: 'incomplete contact',   phone_numbers: [] ) }
+  let(:contacts)    { [contact1, contact2] }
+  let(:phonebook)   { Phonebook.new(contacts) }
+
+  describe "methods" do
+    subject{ phonebook }
+  	it { should respond_to(:contact_names) }
+  	it { should respond_to(:contact_phone_numbers) }
+  	it { should respond_to(:contact_name_and_phone_numbers) }
+  	it { should respond_to(:complete_contacts?) }
+  	it { should respond_to(:incomplete_contacts) }    
+  end
+
 
   describe "#contacts" do
     it "returns assigend contacts" do
@@ -14,40 +24,54 @@ describe Phonebook do
     end
   end
 
-  describe "list_names" do
-	it { expect(phonebook).to respond_to(:list_names) }
-  	it "should return a list with contact names" do
-		expect(phonebook.list_names).to eq([c1.name, c2.name])
-  	end
+  describe "contact_names" do
+    it "returns a list with contact names" do
+      expect(phonebook.contact_names).to eq([contact1.name, contact2.name])
+    end
   end
 
-  describe "list_phone_numbers" do
-	it { expect(phonebook).to respond_to(:list_phone_numbers) }
-  	it "should return a list with contact phone numbers" do
-		expect(phonebook.list_phone_numbers).to eq([c1.phone_numbers, c2.phone_numbers])
-  	end
+  describe "contact_phone_numbers" do
+    it "returns a list with all contact's phone numbers" do
+      expect(phonebook.contact_phone_numbers).to eq([contact1.phone_numbers[0], contact1.phone_numbers[1], contact2.phone_numbers[0]])
+    end
   end  
 
-  describe "list_complete_contacts" do
-	it { expect(phonebook).to respond_to(:list_complete_contacts) }
-  	it "should return a list with contact phone numbers" do
-		expect(phonebook.list_complete_contacts).to eq({c1.name => c1.phone_numbers, c2.name => c2.phone_numbers})
-  	end
+  describe "contact_name_and_phone_numbers" do
+    it "returns a dictionary with contact names and phone numbers" do
+      expect(phonebook.contact_name_and_phone_numbers).to eq({contact1.name => contact1.phone_numbers, contact2.name => contact2.phone_numbers})
+    end
   end
 
   describe "complete_contacts?" do
-	it { expect(phonebook).to respond_to(:complete_contacts?) }
-  	it "should return a list with contact phone numbers" do
-		expect(phonebook.complete_contacts?).to be(true)
-  	end
+    describe "with complete contacts" do
+      it "returns true" do
+        expect(phonebook.complete_contacts?).to be_true
+      end      
+    end
+
+    describe "with incomplete contacts" do
+      let(:contacts) { [contact1, contact2, incomplete] }
+      let(:phonebook) { Phonebook.new(contacts) }
+      it "returns false" do
+        expect(phonebook.complete_contacts?).to be_false
+      end
+    end
   end
 
   describe "incomplete_contacts" do
-  	let(:c3) { double('contact', name: 'third contact', phone_numbers:'') }
-  	before { contacts.push(c3) }
-	it { expect(phonebook).to respond_to(:incomplete_contacts) }
-  	it "should return a list with contact phone numbers" do
-		expect(phonebook.incomplete_contacts).to eq(['third contact'])
-  	end
+    describe "with complete contacts" do
+      it "returns an empty list " do
+        expect(phonebook.incomplete_contacts).to eq([])
+      end
+    end
+
+    describe "with incomplete contacts" do
+      let(:contacts) { [contact1,contact2,incomplete] }
+      let(:phonebook) { Phonebook.new(contacts) }
+      it "returns a list " do
+        expect(phonebook.incomplete_contacts).to eq([incomplete])
+      end
+    end
+
   end
 end
