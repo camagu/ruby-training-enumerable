9ad7f9888256825e2d0ad44bf04ee78fab42a141
jonny gamba <jonny.gamba@globant.com>
2014-02-09 13:49:23 -0500

    exercise completed

--- lib/phonebook.rb
+++ lib/phonebook.rb
@@ -4,4 +4,28 @@ class Phonebook
   def initialize(contacts)
     @contacts = contacts
   end
+
+  def contacts_names
+    @contacts.map(&:name)
+  end
+
+  def contacts_numbers
+    @contacts.map(&:phone_numbers)
+  end
+
+  def contacts_info
+    data = Hash.new
+    @contacts.map do |contact|
+      data << [contact.name, contact.phone_numbers]
+    end
+    data
+  end
+
+  def every_contact_has_number?
+    @contacts.all? { |contact| !contact.phone_numbers.nil?}
+  end
+
+  def contacts_with_no_number
+    @contacts.select { |contact| contact.phone_numbers.nil? }
+  end
 end
--- spec/lib/phonebook_spec.rb
+++ spec/lib/phonebook_spec.rb
@@ -1,13 +1,34 @@
 require 'spec_helper'
 require './lib/phonebook'
+require './lib/contact'
 
 describe Phonebook do
-  let(:contacts)  { [double('first contact'), double('second contact')] }
+  let(:contact1)  { double('first contact', name: 'first name', phone_numbers: '3217889909') }
+  let(:contact2)  { double('second contact', name: 'second name', phone_numbers: '32456563') }
+  let(:contacts)  { [contact1, contact2] }
   let(:phonebook) { Phonebook.new(contacts) }
 
   describe "#contacts" do
     it "returns assigend contacts" do
       expect(phonebook.contacts).to eq(contacts)
+    end
+  end
+
+  describe "contact_names" do
+    it "returns list of names" do
+      expect(phonebook.contacts_names).to eq([contact1.name, contact2.name])
+    end
+  end
+
+  describe "contact_numbers" do
+    it "returns list of contact's numbers" do
+      expect(phonebook.contacts_numbers).to eq([contact1.phone_numbers, contact2.phone_numbers])
+    end
+  end
+
+  describe "every_contact_has_number" do
+    it "returns that every contact has numbers" do
+      expect(phonebook.every_contact_has_number?).to be_true
     end
   end
 end
