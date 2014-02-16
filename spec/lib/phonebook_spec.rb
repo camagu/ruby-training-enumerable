fb3bf45ae8e9745adc7b184150599e94e96724f8
jonny gamba <jonny.gamba@globant.com>
2014-02-16 15:53:55 -0500

    specs updated

--- spec/lib/phonebook_spec.rb
+++ spec/lib/phonebook_spec.rb
@@ -3,8 +3,8 @@ require './lib/phonebook'
 require './lib/contact'
 
 describe Phonebook do
-  let(:contact1)  { double('first contact', name: 'first name', phone_numbers: '3217889909') }
-  let(:contact2)  { double('second contact', name: 'second name', phone_numbers: '32456563') }
+  let(:contact1)  { double('first contact', name: 'first name', phone_numbers: ['3217889909', '6733']) }
+  let(:contact2)  { double('second contact', name: 'second name', phone_numbers: ['32456563']) }
   let(:contacts)  { [contact1, contact2] }
   let(:phonebook) { Phonebook.new(contacts) }
 
@@ -14,19 +14,19 @@ describe Phonebook do
     end
   end
 
-  describe "contact_names" do
+  describe "#contact_names" do
     it "returns list of names" do
       expect(phonebook.contacts_names).to eq([contact1.name, contact2.name])
     end
   end
 
-  describe "contact_numbers" do
+  describe "#contact_phone_numbers" do
     it "returns list of contact's numbers" do
       expect(phonebook.contacts_numbers).to eq([contact1.phone_numbers, contact2.phone_numbers])
     end
   end
 
-  describe "every_contact_has_number" do
+  describe "#every_contact_has_number" do
     it "returns that every contact has numbers" do
       expect(phonebook.every_contact_has_number?).to be_true
     end
