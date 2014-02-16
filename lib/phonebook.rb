01064e3786619c4ac42c09210d2b8da8fed9b564
jonny gamba <jonny.gamba@globant.com>
2014-02-16 15:17:34 -0500

    @ removed because it is not needed

--- lib/phonebook.rb
+++ lib/phonebook.rb
@@ -1,31 +1,31 @@
-class Phonebook
+
   attr_reader :contacts
 
   def initialize(contacts)
-    @contacts = contacts
+    contacts = contacts
   end
 
   def contacts_names
-    @contacts.map(&:name)
+    contacts.map(&:name)
   end
 
   def contacts_numbers
-    @contacts.map(&:phone_numbers)
+    contacts.map(&:phone_numbers)
   end
 
   def contacts_info
     data = Hash.new
-    @contacts.map do |contact|
+    contacts.map do |contact|
       data << [contact.name, contact.phone_numbers]
     end
     data
   end
 
   def every_contact_has_number?
-    @contacts.all? { |contact| !contact.phone_numbers.nil?}
+    contacts.all? { |contact| !contact.phone_numbers.nil?}
   end
 
   def contacts_with_no_number
-    @contacts.select { |contact| contact.phone_numbers.nil? }
+    contacts.select { |contact| contact.phone_numbers.nil? }
   end
 end
